33c1ff2b14da1d9588f0d564b76c0b8b96843a84
saman <abdallaelsaman67@gmail.com>
2019-02-03 02:38:18 +0200

    problem two LL reverse Recursive

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,10 @@
 class Solution {
-	// Iterative sol
+	// Recursive sol
 	public ListNode reverseList(ListNode head) {
-		if (head == null)
-			return null;
-		ListNode p1 = null;
-		ListNode p2 = head;
-		ListNode p3 = head.next;
-		while (p3 != null) {
-			p2.next = p1;
-			if (p3 == null) {
-				break;
-			}
-			p1 = p2;
-			p2 = p3;
-			p3 = p3.next;
-		}
-		return p2;
+		if (head == null || head.next == null) return head;
+		ListNode p = reverseList(head.next);
+		head.next.next = head;
+		head.next = null;
+		return p;
 	}
 }
