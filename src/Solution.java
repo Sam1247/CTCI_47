ede2fbdcfc7318c2df111c2ff6850bef27049ead
saman <abdallaelsaman67@gmail.com>
2019-02-05 01:54:51 +0200

    Intersection of Two Linked Lists O(N)space

--- src/Solution.java
+++ src/Solution.java
@@ -1,142 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
 
-
-// note you can reverse the second half ana attach it so that the linkedList never be modified
-
-class Solution {
-	public boolean isPalindrome(ListNode head) {
-		if (head == null) return true;
-		ListNode current = head;
-		ListNode slow = head;
-		ListNode fast = head;
-		while (fast.next != null) {
-			fast = fast.next.next;
-			slow = slow.next;
-			if (fast == null) break;
+public class Solution {
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if (headA == null || headB == null) return null;
+		Set<ListNode> hash = new HashSet<>();
+		ListNode current1 = headA;
+		ListNode current2 = headB;
+		while (current1 != null) {
+			hash.add(current1);
+			current1 = current1.next;
 		}
-		ListNode reverse = reverseList(slow);
-		while (reverse != null) {
-			if (reverse.val != current.val) {
-				return false;
+		while (current2 != null) {
+			if (hash.contains(current2)) {
+				return current2;
 			}
-			reverse = reverse.next;
-			current = current.next;
+			current2 = current2.next;
 		}
-		return true;
-	}
-	public ListNode reverseList(ListNode head) {
-		ListNode prev = null;
-		ListNode curr = head;
-		while (curr != null) {
-			ListNode nextTemp = curr.next;
-			curr.next = prev;
-			prev = curr;
-			curr = nextTemp;
-		}
-		return prev;
+		return null;
 	}
 }
-
-// recursion solution g4gs
-
-/* Java program to check if linked list is palindrome recursively */
-
-//class LinkedList
-//{
-//	Node head;  // head of list
-//	Node left;
-//
-//	/* Linked list Node*/
-//	class Node
-//	{
-//		char data;
-//		Node next;
-//
-//		Node(char d)
-//		{
-//			data = d;
-//			next = null;
-//		}
-//	}
-//
-//	// Initial parameters to this function are &head and head
-//	boolean isPalindromeUtil(Node right)
-//	{
-//		left = head;
-//
-//		/* stop recursion when right becomes NULL */
-//		if (right == null)
-//			return true;
-//
-//        /* If sub-list is not palindrome then no need to
-//           check for current left and right, return false */
-//		boolean isp = isPalindromeUtil(right.next);
-//		if (isp == false)
-//			return false;
-//
-//		/* Check values at current left and right */
-//		boolean isp1 = (right.data == (left).data);
-//
-//		/* Move left to next node */
-//		left = left.next;
-//
-//		return isp1;
-//	}
-//
-//	// A wrapper over isPalindromeUtil()
-//	boolean isPalindrome(Node head)
-//	{
-//		boolean result = isPalindromeUtil(head);
-//		return result;
-//	}
-//
-//	/* Push a node to linked list. Note that this function
-//	   changes the head */
-//	public void push(char new_data)
-//	{
-//        /* Allocate the Node &
-//           Put in the data */
-//		Node new_node = new Node(new_data);
-//
-//		/* link the old list off the new one */
-//		new_node.next = head;
-//
-//		/* Move the head to point to new Node */
-//		head = new_node;
-//	}
-//
-//	// A utility function to print a given linked list
-//	void printList(Node ptr)
-//	{
-//		while (ptr != null)
-//		{
-//			System.out.print(ptr.data + "->");
-//			ptr = ptr.next;
-//		}
-//		System.out.println("NULL");
-//	}
-//
-//	/* Driver program to test the above functions */
-//	public static void main(String[] args)
-//	{
-//		/* Start with the empty list */
-//		LinkedList llist = new LinkedList();
-//
-//		char str[] = {'a', 'b', 'a', 'c', 'a', 'b', 'a'};
-//		String string = new String(str);
-//		for (int i = 0; i < 7; i++)
-//		{
-//			llist.push(str[i]);
-//			llist.printList(llist.head);
-//			if (llist.isPalindrome(llist.head) != false)
-//			{
-//				System.out.println("Is Palindrome");
-//				System.out.println("");
-//			}
-//			else
-//			{
-//				System.out.println("Not Palindrome");
-//				System.out.println("");
-//			}
-//		}
-//	}
-//}
