11aec9562c160c4fc7c9f6bb7d9f0e9f431a02fe
saman <abdallaelsaman67@gmail.com>
2019-02-04 07:15:13 +0200

    problem if palindrome o(n) space

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,18 @@
+import java.util.ArrayList;
+
 class Solution {
-	public ListNode removeElements(ListNode head, int val) {
-		if (head == null) return null;
-		while (head.val == val) {
-			head = head.next;
-			if (head == null) {
-				return null;
-			}
-		}
+	public boolean isPalindrome(ListNode head) {
+		ArrayList<ListNode> arr = new ArrayList<>();
 		ListNode current = head;
-		while (current.next != null) {
-			if (current.next.val == val) {
-				current.next = current.next.next;
-			} else {
-				current = current.next;
+		while (current != null) {
+			arr.add(current);
+			current = current.next;
+		}
+		for (int i = 0; i < arr.size()/2; ++i) {
+			if (arr.get(i).val != arr.get(arr.size() - 1 - i).val) {
+				return false;
 			}
 		}
-		return head;
+		return true;
 	}
 }
