8c76ff3b60fb592d91c8718333a2825452ff691e
saman <abdallaelsaman67@gmail.com>
2019-02-04 07:44:03 +0200

    problem reverse LL iterative o(1) spaceTime

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,24 @@
 class Solution {
+	public boolean isPalindrome(ListNode head) {
+		if (head == null) return true;
+		ListNode current = head;
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+			if (fast == null) break;
+		}
+		ListNode reverse = reverseList(slow);
+		while (reverse != null) {
+			if (reverse.val != current.val) {
+				return false;
+			}
+			reverse = reverse.next;
+			current = current.next;
+		}
+		return true;
+	}
 	public ListNode reverseList(ListNode head) {
 		ListNode prev = null;
 		ListNode curr = head;
