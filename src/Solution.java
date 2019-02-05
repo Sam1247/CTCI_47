2414be7cabd01248a8c5b51bdb5f17917cf32e15
saman <abdallaelsaman67@gmail.com>
2019-02-05 02:14:48 +0200

    Intersection of Two Linked Lists O(1)space my solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,31 @@
-import java.util.HashSet;
-import java.util.Set;
-
 public class Solution {
 	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 		if (headA == null || headB == null) return null;
-		Set<ListNode> hash = new HashSet<>();
+		int l1 = 0, l2 = 0;
 		ListNode current1 = headA;
 		ListNode current2 = headB;
 		while (current1 != null) {
-			hash.add(current1);
+			l1++;
 			current1 = current1.next;
 		}
 		while (current2 != null) {
-			if (hash.contains(current2)) {
-				return current2;
+			l2++;
+			current2 = current2.next;
+		}
+		current1 = headA;
+		current2 = headB;
+		if (l1 > l2) {
+			for (int i = 0; i <(l1-l2); ++i) {
+				current1 = current1.next;
 			}
+		} else {
+			for (int i = 0; i <(l2-l1); ++i) {
+				current2 = current2.next;
+			}
+		}
+		while (current1 != null) {
+			if (current1 == current2) return current1;
+			current1 = current1.next;
 			current2 = current2.next;
 		}
 		return null;
