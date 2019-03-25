149eb674bfeb64d40e810fef9bb6b40c762e3462
saman <abdallaelsaman67@gmail.com>
2019-03-25 13:09:02 +0200

    Range Sum of BST Best solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,13 @@
-import java.util.ArrayList;
-
 class Solution {
-	ArrayList<Integer> arrayList = new ArrayList<>();
-	public int getMinimumDifference(TreeNode root) {
-		inorder(root);
-		int min = arrayList.get(1)-arrayList.get(0);
-		for (int i = 1; i < arrayList.size()-1; ++i) {
-			if ((arrayList.get(i+1)-arrayList.get(i)) < min) {
-				min = (arrayList.get(i+1)-arrayList.get(i));
-			}
-		}
-		return min;
-	}
-
-	void inorder (TreeNode root) {
+	int sum = 0;
+	public int rangeSumBST(TreeNode root, int L, int R) {
 		if (root != null) {
-			inorder(root.left);
-			arrayList.add(root.val);
-			inorder(root.right);
+			rangeSumBST(root.left, L, R);
+			if (root.val >= L && root.val <= R) {
+				sum += root.val;
+			}
+			rangeSumBST(root.right, L, R);
 		}
+		return sum;
 	}
 }
-//class Solution {
-//	private int minv = Integer.MAX_VALUE;
-//	private Integer prev = null;
-//	public int getMinimumDifference(TreeNode root) {
-//		if(root == null) return minv;
-//		getMinimumDifference(root.left);
-//		if(prev != null){
-//			minv = Math.min(root.val - prev, minv);
-//		}
-//		prev = root.val;
-//		getMinimumDifference(root.right);
-//		return minv;
-//	}
-//}
