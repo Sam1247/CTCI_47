ea7b286ce6368576e6c615a8881937418c47c2cb
saman <abdallaelsaman67@gmail.com>
2019-04-09 20:54:17 +0200

    remove Duplicates (two pointers)

--- src/Solution.java
+++ src/Solution.java
@@ -1,39 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-	public boolean canFinish(int numCourses, int[][] prerequisites) {
-
-		List<Integer>[] graph= new ArrayList[numCourses];
-
-		for(int i=0;i<numCourses;i++){
-			graph[i]= new ArrayList<>();
-		}
-		for(int[] dep: prerequisites){
-			graph[dep[0]].add(dep[1]);
-		}
-
-		int[] visited= new int[numCourses];
-		for(int i=0;i<numCourses;i++){
-			if(visited[i]==0){
-
-				if( hasCycle(graph, i ,visited)) return false;
+	public int removeDuplicates(int[] nums) {
+		int p1 = 0;
+		int p2 = 1;
+		while (p2 < nums.length) {
+			if (nums[p2] == nums[p1]) {
+				p2++;
+			} else {
+				p1++;
+				nums[p1] = nums[p2];
+				p2++;
 			}
 		}
-
-		return true;
-	}
-
-	boolean hasCycle( List<Integer>[] graph,int node, int[] visited){
-		if(visited[node]==1) return true;
-
-		visited[node]=1;
-		for(int i=0;i<graph[node].size();i++){
-			if(visited[graph[node].get(i)]!=2){
-				if(hasCycle(graph, graph[node].get(i),visited)) return true;
-			}
-		}
-		visited[node]=2;
-		return false;
+		return p1+1;
 	}
 }
