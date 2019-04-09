3e90072a5e230109945c3829ab50dedc9775c225
saman <abdallaelsaman67@gmail.com>
2019-04-09 15:26:38 +0200

    429. N-ary Tree Level Order Traversal DFS solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,7 @@
-import java.util.LinkedList;
-import java.util.List;
-
 class Solution {
+	public boolean leafSimilar(TreeNode root1, TreeNode root2) {
 
-	List<List<Integer>> lists;
 
-	public List<List<Integer>> levelOrder(Node root) {
-		lists = new LinkedList<>();
-		handler(root, 0);
-		return lists;
-	}
-
-	public void handler (Node root, int level) {
-		if (root == null) return;
-		if (level >= lists.size()) lists.add(new LinkedList<>());
-		lists.get(level).add(root.val);
-		for (Node node: root.children) {
-			handler(node, level+1);
-		}
+		return true;
 	}
 }
