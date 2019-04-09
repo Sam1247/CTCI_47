30a3223df903105ef3b35154a60239d2e395cc43
saman <abdallaelsaman67@gmail.com>
2019-04-09 18:02:42 +0200

    207. Course Schedule (modified 1 try)

--- src/Solution.java
+++ src/Solution.java
@@ -6,8 +6,10 @@ class Solution {
 	boolean cyclic = true;
 	ArrayList<Integer>[] adj;
 	Set<Integer> set = new HashSet<Integer>();
+	boolean [] isVisited;
 	public boolean canFinish(int numCourses, int[][] prerequisites) {
 		adj = new ArrayList[numCourses];
+		isVisited = new boolean[numCourses];
 		for (int i = 0; i < numCourses; i++) {
 			adj[i] = new ArrayList<>();
 		}
@@ -15,13 +17,18 @@ class Solution {
 			adj[prerequisites[i][0]].add(prerequisites[i][1]);
 		}
 		for (int i = 0; i < numCourses; ++i) {
-			DFS(i);
+			if (!isVisited[i])
+				DFS(i);
+			if (!cyclic) {
+				return false;
+			}
 		}
 		return cyclic;
 	}
 
 	public void DFS (int s) {
 		set.add(s);
+		isVisited[s] = true;
 		for (int u: adj[s]) {
 			if (set.contains(u)) {
 				cyclic = false;
