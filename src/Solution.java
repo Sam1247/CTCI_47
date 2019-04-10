9b5eac51224da4d15081f67e351f6dac25a12188
saman <abdallaelsaman67@gmail.com>
2019-04-10 20:54:48 +0200

    104. Maximum Depth of Binary Tree (trivial)

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,15 @@
-class Solution(object):
-		def singleNumber(self, nums):
-		"""
-		:type nums: List[int]
-		:rtype: int
-		"""
-		return 2 * sum(set(nums)) - sum(nums)
+class Solution {
+	int len = 0;
+	public int maxDepth(TreeNode root) {
+		if (root == null) return 0;
+		DFS(root, 1);
+		return len;
+	}
+
+	void DFS (TreeNode root, int dep) {
+		if (root == null) return;
+		if (len < dep) len = dep;
+		DFS(root.left, dep+1);
+		DFS(root.right, dep+1);
+	}
+}
