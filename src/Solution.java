a5a7a5ea908c3e3eca68a4e31f82408576956a33
saman <abdallaelsaman67@gmail.com>
2019-04-10 15:59:33 +0200

    122. Best Time to Buy and Sell Stock II (optimal solution)

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,10 @@
 class Solution {
 	public int maxProfit(int[] prices) {
-		int sum = 0;
-		int i = 0;
-		int j = 1;
-		while (j < prices.length) {
-			//
-			if (prices[i] > prices[j]) {
-				i = j;
-				j++;
-			} else {
-				while (j < prices.length && prices[j-1] < prices[j]) {
-					j++;
-				}
-				sum += prices[j-1] - prices[i];
-				i = j;
-				j++;
-			}
+		int maxprofit = 0;
+		for (int i = 1; i < prices.length; i++) {
+			if (prices[i] > prices[i - 1])
+				maxprofit += prices[i] - prices[i - 1];
 		}
-		return sum;
+		return maxprofit;
 	}
 }
